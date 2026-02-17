ae83da511b5e264268845496d2e54df0ca3be055
saitejareddyy <saitejareddy0502@gmail.com>
2026-02-17 10:30:58 +0530

    LSP-Rules , Signaturerules, MethodArgumentRule

---
+++ SOLID PRINCIPLES/LSP-Rules/SignatureRules/MethodArgumentRule.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Parent {
+public:
+    virtual void print(string msg){
+        cout<<"Parent " << msg << endl;
+    }
+};
+
+class Child : public Parent {
+public:
+    void print(string msg) override{
+        cout<<"Child " << msg << endl;
+    }
+};
+
+class Client {
+private:
+    Parent* p;
+public:
+    Client(Parent* p){
+        this->p = p;
+    }
+    void printMsg(string msg){
+        p->print(msg);
+    }
+};
+
+int main(){
+    Parent* parent = new Parent();
+    Parent* child = new Child();
+    // Client* client1 = new Client(parent);
+    Client* client2 = new Client(child); 
+    // client1->printMsg("Hello");
+    client2->printMsg("Hello");
+    return 0;
+}
